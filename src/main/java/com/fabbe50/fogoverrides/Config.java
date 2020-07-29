aa3e8239c880f16536fd95eb3b17a2795b609aa3
William <fabbe50@users.noreply.github.com>
2020-07-29 15:45:19 +0200

    Void Fog and Particles

--- src/main/java/com/fabbe50/fogoverrides/Config.java
+++ src/main/java/com/fabbe50/fogoverrides/Config.java
@@ -11,9 +11,17 @@ public class Config {
     }
 
     private General general;
+    private Overworld overworld;
+    private Nether nether;
+    private Lava lava;
+    private Water water;
 
     public Config() {
         general = new General();
+        overworld = new Overworld();
+        nether = new Nether();
+        lava = new Lava();
+        water = new Water();
 
         spec = builder.build();
     }
@@ -22,15 +30,23 @@ public class Config {
         return general;
     }
 
+    public Overworld getOverworld() {
+        return overworld;
+    }
+
+    public Nether getNether() {
+        return nether;
+    }
+
+    public Lava getLava() {
+        return lava;
+    }
+
+    public Water getWater() {
+        return water;
+    }
+
     public class General {
-        ForgeConfigSpec.BooleanValue netherFogRemove;
-        ForgeConfigSpec.DoubleValue netherFogDistance;
-        ForgeConfigSpec.BooleanValue lavaFogRemove;
-        ForgeConfigSpec.DoubleValue lavaFogDistance;
-        ForgeConfigSpec.BooleanValue waterFogRemove;
-        ForgeConfigSpec.DoubleValue waterFogDistance;
-        ForgeConfigSpec.BooleanValue overworldFogRemove;
-        ForgeConfigSpec.DoubleValue overworldFogDistance;
         ForgeConfigSpec.BooleanValue potionAffectsVision;
         ForgeConfigSpec.BooleanValue creativeOverrides;
         ForgeConfigSpec.BooleanValue disableFireOverlay;
@@ -40,14 +56,6 @@ public class Config {
         public General() {
             builder.push("general");
 
-            netherFogRemove = builder.comment("Disable Nether Fog?").define("Disable Nether Fog", false);
-            netherFogDistance = builder.comment("Nether Fog Density").defineInRange("Nether Fog Density", 0.01, 0.001, 0.5);
-            lavaFogRemove = builder.comment("Disable Lava Fog?").define("Disable Lava Fog", false);
-            lavaFogDistance = builder.comment("Lava Fog Density").defineInRange("Lava Fog Density", 0.03, 0.001, 0.5);
-            waterFogRemove = builder.comment("Disable Water Fog?").define("Disable Water Fog", false);
-            waterFogDistance = builder.comment("Water Fog Density").defineInRange("Water Fog Density", 0.01, 0.001, 0.5);
-            overworldFogRemove = builder.comment("Disable Overworld Fog?").define("Disable Overworld Fog", false);
-            overworldFogDistance = builder.comment("Overworld Fog Density").defineInRange("Overworld Fog Density", 0.0015, 0.001, 0.5);
             potionAffectsVision = builder.comment("Overrides are dependent on potion effects").define("Potion Active", true);
             creativeOverrides = builder.comment("Creative Mode Always Removes Fog").define("Creative Overrides", true);
             disableFireOverlay = builder.comment("Disable the fire overlay when you're burning").define("Disable Fire Overlay", false);
@@ -55,38 +63,6 @@ public class Config {
             disableWaterOverlay = builder.comment("Disable the water overlay when you're in water").define("Disable Water Overlay", false);
 
             builder.pop();
-        }
-
-        public ForgeConfigSpec.BooleanValue getNetherFogRemove() {
-            return netherFogRemove;
-        }
-
-        public ForgeConfigSpec.DoubleValue getNetherFogDistance() {
-            return netherFogDistance;
-        }
-
-        public ForgeConfigSpec.BooleanValue getLavaFogRemove() {
-            return lavaFogRemove;
-        }
-
-        public ForgeConfigSpec.DoubleValue getLavaFogDistance() {
-            return lavaFogDistance;
-        }
-
-        public ForgeConfigSpec.BooleanValue getWaterFogRemove() {
-            return waterFogRemove;
-        }
-
-        public ForgeConfigSpec.DoubleValue getWaterFogDistance() {
-            return waterFogDistance;
-        }
-
-        public ForgeConfigSpec.BooleanValue getOverworldFogRemove() {
-            return overworldFogRemove;
-        }
-
-        public ForgeConfigSpec.DoubleValue getOverworldFogDistance() {
-            return overworldFogDistance;
         }
 
         public ForgeConfigSpec.BooleanValue getPotionAffectsVision() {
@@ -107,6 +83,120 @@ public class Config {
 
         public ForgeConfigSpec.BooleanValue getDisableWaterOverlay() {
             return disableWaterOverlay;
+        }
+
+
+    }
+
+    public class Overworld {
+        ForgeConfigSpec.BooleanValue overworldFogRemove;
+        ForgeConfigSpec.DoubleValue overworldFogDistance;
+        ForgeConfigSpec.BooleanValue enableVoidFog;
+        ForgeConfigSpec.DoubleValue voidFogDensity;
+        ForgeConfigSpec.IntValue yLevelActivate;
+        ForgeConfigSpec.BooleanValue enableVoidParticles;
+
+        public Overworld() {
+            builder.push("overworld");
+
+            overworldFogRemove = builder.comment("Disable Overworld Fog?").define("Disable Overworld Fog", false);
+            overworldFogDistance = builder.comment("Overworld Fog Density").defineInRange("Overworld Fog Density", 0.0015, 0.001, 0.5);
+            enableVoidFog = builder.comment("Readds the void fog that was removed in 1.8").define("Enable Void Fog", false);
+            voidFogDensity = builder.comment("This is so you can adjust the density of the fog").defineInRange("Void Fog Density", 0.3, 0.001, 0.5);
+            yLevelActivate = builder.comment("At what Y-level should the fog active on").defineInRange("Y-Level Activation", 16, 0, 255);
+            enableVoidParticles = builder.comment("Readds the void particles that was removed in 1.8").define("Enable Void Particles", false);
+
+            builder.pop();
+        }
+
+        public ForgeConfigSpec.BooleanValue getOverworldFogRemove() {
+            return overworldFogRemove;
+        }
+
+        public ForgeConfigSpec.DoubleValue getOverworldFogDistance() {
+            return overworldFogDistance;
+        }
+
+        public ForgeConfigSpec.BooleanValue getEnableVoidFog() {
+            return enableVoidFog;
+        }
+
+        public ForgeConfigSpec.DoubleValue getVoidFogDensity() {
+            return voidFogDensity;
+        }
+
+        public ForgeConfigSpec.IntValue getyLevelActivate() {
+            return yLevelActivate;
+        }
+
+        public ForgeConfigSpec.BooleanValue getEnableVoidParticles() {
+            return enableVoidParticles;
+        }
+    }
+
+    public class Nether {
+        ForgeConfigSpec.BooleanValue netherFogRemove;
+        ForgeConfigSpec.DoubleValue netherFogDistance;
+
+        public Nether() {
+            builder.push("nether");
+
+            netherFogRemove = builder.comment("Disable Nether Fog?").define("Disable Nether Fog", false);
+            netherFogDistance = builder.comment("Nether Fog Density").defineInRange("Nether Fog Density", 0.01, 0.001, 0.5);
+
+            builder.pop();
+        }
+
+        public ForgeConfigSpec.BooleanValue getNetherFogRemove() {
+            return netherFogRemove;
+        }
+
+        public ForgeConfigSpec.DoubleValue getNetherFogDistance() {
+            return netherFogDistance;
+        }
+    }
+
+    public class Lava {
+        ForgeConfigSpec.BooleanValue lavaFogRemove;
+        ForgeConfigSpec.DoubleValue lavaFogDistance;
+
+        public Lava() {
+            builder.push("lava");
+
+            lavaFogRemove = builder.comment("Disable Lava Fog?").define("Disable Lava Fog", false);
+            lavaFogDistance = builder.comment("Lava Fog Density").defineInRange("Lava Fog Density", 0.03, 0.001, 0.5);
+
+            builder.pop();
+        }
+
+        public ForgeConfigSpec.BooleanValue getLavaFogRemove() {
+            return lavaFogRemove;
+        }
+
+        public ForgeConfigSpec.DoubleValue getLavaFogDistance() {
+            return lavaFogDistance;
+        }
+    }
+
+    public class Water {
+        ForgeConfigSpec.BooleanValue waterFogRemove;
+        ForgeConfigSpec.DoubleValue waterFogDistance;
+
+        public Water() {
+            builder.push("water");
+
+            waterFogRemove = builder.comment("Disable Water Fog?").define("Disable Water Fog", false);
+            waterFogDistance = builder.comment("Water Fog Density").defineInRange("Water Fog Density", 0.01, 0.001, 0.5);
+
+            builder.pop();
+        }
+
+        public ForgeConfigSpec.BooleanValue getWaterFogRemove() {
+            return waterFogRemove;
+        }
+
+        public ForgeConfigSpec.DoubleValue getWaterFogDistance() {
+            return waterFogDistance;
         }
     }
 }
--- src/main/java/com/fabbe50/fogoverrides/FogOverrides.java
+++ src/main/java/com/fabbe50/fogoverrides/FogOverrides.java
@@ -1,37 +1,32 @@
 package com.fabbe50.fogoverrides;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.Effects;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.tags.Tag;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldType;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderBlockOverlayEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 @Mod("fogoverrides")
 @Mod.EventBusSubscriber
@@ -56,26 +51,76 @@ public class FogOverrides {
             event.setDensity(0);
             event.setCanceled(true);
         } else if (checkFluidConditions(player, FluidTags.LAVA, Effects.FIRE_RESISTANCE)) {
-            event.setDensity(config.getGeneral().getLavaFogDistance().get().floatValue());
-            if (config.getGeneral().getLavaFogRemove().get())
+            event.setDensity(config.getLava().getLavaFogDistance().get().floatValue());
+            if (config.getLava().getLavaFogRemove().get())
                 event.setDensity(0);
             event.setCanceled(true);
         } else if (checkFluidConditions(player, FluidTags.WATER, Effects.WATER_BREATHING)) {
-            event.setDensity(config.getGeneral().getWaterFogDistance().get().floatValue());
-            if (config.getGeneral().getWaterFogRemove().get())
+            event.setDensity(config.getWater().getWaterFogDistance().get().floatValue());
+            if (config.getWater().getWaterFogRemove().get())
                 event.setDensity(0);
             event.setCanceled(true);
         } else if (!(player.areEyesInFluid(FluidTags.LAVA) || player.areEyesInFluid(FluidTags.WATER))) {
             if (checkDimensionConditions(player, DimensionType.OVERWORLD)) {
-                event.setDensity(config.getGeneral().getOverworldFogDistance().get().floatValue());
-                if (config.getGeneral().getOverworldFogRemove().get())
+                event.setDensity(config.getOverworld().getOverworldFogDistance().get().floatValue());
+                if (config.getOverworld().getOverworldFogRemove().get())
                     event.setDensity(0);
+                if (config.getOverworld().getEnableVoidFog().get() && player.getPosition().getY() < (config.getOverworld().getyLevelActivate().get()))
+                    event.setDensity(config.getOverworld().getVoidFogDensity().get().floatValue());
                 event.setCanceled(true);
             } else if (checkDimensionConditions(player, DimensionType.THE_NETHER)) {
-                event.setDensity(config.getGeneral().getNetherFogDistance().get().floatValue());
-                if (config.getGeneral().getNetherFogRemove().get())
+                event.setDensity(config.getNether().getNetherFogDistance().get().floatValue());
+                if (config.getNether().getNetherFogRemove().get())
                     event.setDensity(0);
                 event.setCanceled(true);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public static void colorAdjust(EntityViewRenderEvent.FogColors event) {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        if (player == null) {
+            return;
+        }
+
+        if (((player.isCreative() || player.isSpectator()) && config.getGeneral().getCreativeOverrides().get())) {}
+        else if (player.getPosition().getY() < config.getOverworld().getyLevelActivate().get() && config.getOverworld().getEnableVoidFog().get() && checkDimensionConditions(player, DimensionType.OVERWORLD)) {
+            event.setRed(0);
+            event.setGreen(0);
+            event.setBlue(0);
+        }
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public static void particles(TickEvent.WorldTickEvent event) {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        World world = Minecraft.getInstance().world;
+        if (player == null || world == null)
+            return;
+
+        if (((player.isCreative() || player.isSpectator()) && config.getGeneral().getCreativeOverrides().get())) {}
+        else if (player.getPosition().getY() < config.getOverworld().getyLevelActivate().get() && config.getOverworld().getEnableVoidParticles().get() && checkDimensionConditions(player, DimensionType.OVERWORLD)) {
+            int x = MathHelper.floor(player.getPosX());
+            int y = MathHelper.floor(player.getPosY());
+            int z = MathHelper.floor(player.getPosZ());
+            byte b = config.getOverworld().getyLevelActivate().get().byteValue();
+
+            for (int i = 0; i < 1000; i++) {
+                int j = x + world.rand.nextInt(b) - world.rand.nextInt(b);
+                int k = y + world.rand.nextInt(b) - world.rand.nextInt(b);
+                int l = z + world.rand.nextInt(b) - world.rand.nextInt(b);
+                BlockState block = world.getBlockState(new BlockPos(j, k, l));
+
+                if (block.getMaterial() == Material.AIR) {
+                    if (world.rand.nextInt(8) > k && (world.getWorldType() != WorldType.FLAT && checkDimensionConditions(player, DimensionType.OVERWORLD))) {
+                        float h = k + world.rand.nextFloat();
+                        if (h >= 0 && config.getOverworld().getyLevelActivate().get() > h)
+                            world.addParticle(ParticleTypes.MYCELIUM, j + world.rand.nextFloat(), h, l + world.rand.nextFloat(), 0, 0, 0);
+                    }
+                }
             }
         }
     }
