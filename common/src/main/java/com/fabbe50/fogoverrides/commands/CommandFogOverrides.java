c21166d0b0fff6f34129200890dd0b1535e811bb
William <fabbe50@users.noreply.github.com>
2025-01-03 15:46:33 +0100

    Improvements to Fog Overrides command.
    Now allows for changing config settings without having to edit the config file and restart the server.

--- common/src/main/java/com/fabbe50/fogoverrides/commands/CommandFogOverrides.java
+++ common/src/main/java/com/fabbe50/fogoverrides/commands/CommandFogOverrides.java
@@ -1,5 +1,8 @@
 package com.fabbe50.fogoverrides.commands;
 
+import com.fabbe50.fogoverrides.Log;
+import com.fabbe50.fogoverrides.ModConfig;
+import com.fabbe50.fogoverrides.data.CurrentDataStorage;
 import com.fabbe50.fogoverrides.network.NetworkHandler;
 import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.commands.CommandSourceStack;
@@ -10,10 +13,15 @@ public class CommandFogOverrides {
     public CommandFogOverrides() {}
 
     public static void register(CommandDispatcher<CommandSourceStack> commandDispatcher) {
-        commandDispatcher.register(Commands.literal("fogoverrides").requires(commandSourceStack -> commandSourceStack.hasPermission(2)).then(Commands.literal("reload").executes(context -> reload(context.getSource()))));
+        commandDispatcher.register(
+                Commands.literal("fogoverrides").requires(commandSourceStack -> commandSourceStack.hasPermission(4))
+                        .then(Commands.literal("reload").executes(context -> reload(context.getSource())))
+                        .then(Commands.literal("config").executes(context -> config(context.getSource())))
+        );
     }
 
     private static int reload(CommandSourceStack commandSourceStack) {
+        ModConfig.load(ModConfig.getConfigFile());
         int modUsersUpdated = NetworkHandler.sendSettingsToAllPlayers();
 
         if (modUsersUpdated == 1) {
@@ -23,5 +31,16 @@ public class CommandFogOverrides {
         }
 
         return modUsersUpdated;
+    }
+
+    private static int config(CommandSourceStack commandSourceStack) {
+        if (CurrentDataStorage.INSTANCE.isIntegratedServer()) {
+            commandSourceStack.sendFailure(Component.translatable("text.fogoverrides.commands.config.failure"));
+        }
+        NetworkHandler.openConfigScreenOnClient(commandSourceStack.getPlayer());
+
+        Log.info("Opened Config");
+
+        return 1;
     }
 }
