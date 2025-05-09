9b6f29867ab156af3a1cc6fea55762dc4178fe33
William <fabbe50@users.noreply.github.com>
2025-05-09 16:54:07 +0200

    Fixes + Small improvements
    - Changed how the mod gets the registry data. Now only loads the dimension and biome list once a world has been loaded.
    1. Allows for server side dimensions and biomes to be customized as long as the server has the mod installed.
    2. Fixes an issue where the dimension and biome list wouldn't load with some mod combinations.
    - Server now sends it's registry list to the client on join.
    - Fixed config command opening the config screen on integrated servers.
    - Dimension and Biome list in the config menu is now in alphabetical order.

--- common/src/main/java/com/fabbe50/fogoverrides/network/HandshakePacket.java
+++ common/src/main/java/com/fabbe50/fogoverrides/network/HandshakePacket.java
@@ -3,6 +3,7 @@ package com.fabbe50.fogoverrides.network;
 import com.fabbe50.fogoverrides.ClientUtilities;
 import com.fabbe50.fogoverrides.Log;
 import com.fabbe50.fogoverrides.data.CurrentDataStorage;
+import com.fabbe50.fogoverrides.data.ModRegistry;
 import dev.architectury.networking.NetworkManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -42,6 +43,7 @@ public class HandshakePacket {
                         if (!NetworkHandler.modUsers.contains(context.getPlayer()))
                             NetworkHandler.modUsers.add(context.getPlayer());
                         NetworkManager.sendToPlayer((ServerPlayer) context.getPlayer(), new HandshakePacket.ClientPacket.PacketPayload(true));
+                        NetworkManager.sendToPlayer((ServerPlayer) context.getPlayer(), new RegistryPacket.Client.PacketPayload(ModRegistry.getDimensions(), ModRegistry.getBiomes()));
                         NetworkHandler.sendSettingsToPlayer(context.getPlayer());
                     }
                 });
